98d3efc58f7a5bc47571defa3d5dc5ef33164819
José Gregorio Materán <josemateran1998@gmail.com>
2023-01-07 14:01:57 -0400

    feat: tailwind and font setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import clsx from 'clsx'
+
+import { Inter } from '@next/font/google'
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter'
+})
+
 export default function App ({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={clsx(inter.variable, 'font-inter')}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
