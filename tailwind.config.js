df038216ca71124bea2d0c4489daed64495435d3
José Gregorio Materán <josemateran1998@gmail.com>
2023-01-09 17:41:55 -0400

    feat: ui folder added to tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,8 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}'
+    './components/**/*.{js,ts,jsx,tsx}',
+    './ui/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {
